35ad1dfd3ac8fb4d696f59163e01366df85535ad
Angel Steven Cárdenas <102682105+Angel-Carde-Pro@users.noreply.github.com>
2024-01-13 15:41:16 -0500

    Copia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import{LoginComponent} from './login/login.component';
-import { FormpreguntaasComponent } from './formpreguntaas/formpreguntaas.component';
-import { RegisterComponent } from './register/register.component';
-import { PerfiluserComponent } from './perfiluser/perfiluser.component';
-import { PostulacionesComponent } from './pages/postulaciones/postulaciones.component';
-import { VisualizarcvComponent } from './visualizarcv/visualizarcv.component';
-import { DetallescandidatoComponent } from './detallescandidato/detallescandidato.component';
-import { ParametrospreguntasComponent } from './parametrospreguntas/parametrospreguntas.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InitComponent } from './pages/init/init.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: InitComponent },
+  { path: 'init', component: InitComponent },
   { path: 'home', component: HomeComponent },
-  {path: 'login',component: LoginComponent},
-  { path: 'preguntas', component: FormpreguntaasComponent},
-  {path: 'register',component: RegisterComponent},
-  {path: 'perfil-usuario',component: PerfiluserComponent},
-  {path:'postulaciones',component:PostulacionesComponent},
-  {path: 'curriculum',component:VisualizarcvComponent},
-  {path: 'candidato-details',component:DetallescandidatoComponent},
-  {path:'parametros-preguntas', component:ParametrospreguntasComponent}
-  
- // { path: 'register-provincias', component: RegistroProvinciasComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { FormpreguntaasComponent } from './formpreguntaas/formpreguntaas.component';
-import { PerfiluserComponent } from './perfiluser/perfiluser.component';
-import { PostulacionesComponent } from './pages/postulaciones/postulaciones.component';
-import { VisualizarcvComponent } from './visualizarcv/visualizarcv.component';
 import { SlidebarComponent } from './modules/slidebar/slidebar.component';
-import { DetallescandidatoComponent } from './detallescandidato/detallescandidato.component';
 import { InitComponent } from './pages/init/init.component';
-
+import { ScrollToTopComponent } from './modules/scroll-to-top/scroll-to-top.component';
 
 @NgModule({
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    FormpreguntaasComponent,
-    PerfiluserComponent,
-    PostulacionesComponent,
-    VisualizarcvComponent,
-   // RegistroProvinciasComponent
-   SlidebarComponent,
-   DetallescandidatoComponent,
-   InitComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    SlidebarComponent,
+    InitComponent,
+    ScrollToTopComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
