08e5315647870119f5b291b27e90a05863048861
Lokyre <jose.fares.est@tecazuay.edu.ec>
2024-01-18 08:19:09 -0500

    A

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutBaseComponent } from './layout/layout-client/layout-base.component';
-import { LayoutSystemComponent } from './layout/layout-system/layout-system.component';
+import { HomeComponent } from './home/home.component';
+import{LoginComponent} from './login/login.component';
+import { FormpreguntaasComponent } from './formpreguntaas/formpreguntaas.component';
+import { RegisterComponent } from './register/register.component';
+import { PostulacionesComponent } from './pages/postulaciones/postulaciones.component';
+import { VisualizarcvComponent } from './visualizarcv/visualizarcv.component';
+import { DetallescandidatoComponent } from './detallescandidato/detallescandidato.component';
+import { ParametrospreguntasComponent } from './parametrospreguntas/parametrospreguntas.component';
+import { CompanyDetailsComponent } from './company-details/company-details.component';
 
 const routes: Routes = [
-  {
-    path: 'account',
-    redirectTo: 'account/login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./pages/authentication/authentication.module').then((m) => m.AuthenticationModule)
-  },
-  {
-    path: 'inicio', component: LayoutBaseComponent,
-    children: [
-      { path: '', loadChildren: () => import('./pages/start/start.module').then((m) => m.StartModule) }
-    ]
-  },
-  {
-    path: 'system', component: LayoutSystemComponent,
-    children: [
-      { path: 'alumni', loadChildren: () => import('./pages/alumni/alumni.module').then((m) => m.AlumniModule) },
-      { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then((m) => m.AdminModule) }
-    ], // Steve: Revisar esto: canActivate: [AuthGuard]
-  },
-
-  // Ruta inicial
-  { path: '**', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {path: 'login',component: LoginComponent},
+  { path: 'preguntas', component: FormpreguntaasComponent},
+  {path: 'register',component: RegisterComponent},
+  {path:'postulaciones',component:PostulacionesComponent},
+  {path: 'curriculum',component:VisualizarcvComponent},
+  {path: 'person-details',component:DetallescandidatoComponent},
+  {path:'parametros-preguntas', component:ParametrospreguntasComponent},
+  {path: 'company-details' , component: CompanyDetailsComponent},
+ // { path: 'register-provincias', component: RegistroProvinciasComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
-import { LayoutBaseComponent } from './layout/layout-client/layout-base.component';
-import { LayoutSystemComponent } from './layout/layout-system/layout-system.component';
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormpreguntaasComponent } from './formpreguntaas/formpreguntaas.component';
+import { PostulacionesComponent } from './pages/postulaciones/postulaciones.component';
+import { VisualizarcvComponent } from './visualizarcv/visualizarcv.component';
+import { SlidebarComponent } from './modules/slidebar/slidebar.component';
+import { DetallescandidatoComponent } from './detallescandidato/detallescandidato.component';
+import { InitComponent } from './pages/init/init.component';
+import { CompanyDetailsComponent } from './company-details/company-details.component';
+import { StateListComponent } from './state-list/state-list.component';
 
-export function playerFactory() {
-  return player;
-}
+
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ModalModule.forRoot(), LottieModule.forRoot({ player: playerFactory })],
   declarations: [
     AppComponent,
-    LayoutBaseComponent,
-    LayoutSystemComponent,
-    ScrollToTopComponent,
+    LoginComponent,
+    HomeComponent,
+    FormpreguntaasComponent,
+    PostulacionesComponent,
+    VisualizarcvComponent,
+   SlidebarComponent,
+   DetallescandidatoComponent,
+   InitComponent,
+   CompanyDetailsComponent,
+   StateListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
