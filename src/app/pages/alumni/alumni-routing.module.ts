2aa40811e5a4777ca86e114a740dacd67eaa4d3f
Angel Steven Cárdenas <102682105+Angel-Carde-Pro@users.noreply.github.com>
2024-01-21 16:54:09 -0500

    Antes de unir al main

--- src/app/pages/alumni/alumni-routing.module.ts
+++ src/app/pages/alumni/alumni-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostulacionesComponent } from './postulaciones/postulaciones.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ParametrosPreguntasComponent } from './parametros-preguntas/parametros-preguntas.component';
+import { StateListComponent } from './state-list/state-list.component';
+import { SectorEmpresarialCrudComponent } from './sector-empresarial-crud/sector-empresarial-crud.component';
+import { CarrerasCrudComponent } from './carreras-crud/carreras-crud.component';
+import { OfertaDetalleComponent } from './oferta-detalle/oferta-detalle.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'postulaciones', component: PostulacionesComponent },
-    // { path: 'postulaciones', component: PostulacionesComponent },
+    { path: 'parametros-preguntas', component: ParametrosPreguntasComponent },
+    { path: 'estados', component: StateListComponent },
+    { path: 'carrera', component: CarrerasCrudComponent },
+    { path: 'sector-empresarial', component: SectorEmpresarialCrudComponent },
+    { path: 'oferta-detalle', component: OfertaDetalleComponent },
 ];
 
 @NgModule({
---
+++ src/app/pages/alumni/sector-empresarial-crud/sector-empresarial-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sector-empresarial-crud',
+  templateUrl: './sector-empresarial-crud.component.html',
+  styleUrls: ['./sector-empresarial-crud.component.css']
+})
+export class SectorEmpresarialCrudComponent {
+
+}
