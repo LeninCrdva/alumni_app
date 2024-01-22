574b60ff943659568326906a58a903cab52734c5
Lokyre <jose.fares.est@tecazuay.edu.ec>
2024-01-22 11:18:15 -0500

    Graduados List

---
+++ src/app/pages/alumni/graduados-list/graduados-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graduados-list',
+  templateUrl: './graduados-list.component.html',
+  styleUrls:['./graduados-list.component.css', '../../../../assets/temporal/style.css'] 
+})
+export class GraduadosListComponent {
+
+}
