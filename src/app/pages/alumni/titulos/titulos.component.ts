7034ce1eb09b3113d80d89df9be38e6932079cfd
Angel Steven Cárdenas <102682105+Angel-Carde-Pro@users.noreply.github.com>
2024-01-26 10:58:20 -0500

    sasa

--- src/app/pages/alumni/titulos/titulos.component.ts
+++ src/app/pages/alumni/titulos/titulos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TituloService } from '../../../data/service/titulo.service';
 import { Titulo } from '../../../data/model/titulo';
+import { Graduado } from '../../../data/model/graduado';
+import { Carrera } from '../../../data/model/carrera';
 
 @Component({
   selector: 'app-titulos',
@@ -8,19 +10,15 @@ import { Titulo } from '../../../data/model/titulo';
   styleUrls: ['./titulos.component.css', '../../../../assets/prefabs/headers.css']
 })
 export class TitulosComponent implements OnInit {
+  graduado: Graduado | any = [];
+  carrera: Carrera | any = [];
+
+  titulos: Titulo[] = [];
+  nuevoTitulo: Titulo = { graduado: this.graduado, tipo: '', nivel: '', institucion: false, nombre_titulo: '', fecha_registro: new Date(), fecha_emision: new Date(), num_registro: '', carrera: this.carrera };
+  nuevoTituloCarga: Titulo = { id: 0, graduado: this.graduado, tipo: '', nivel: '', institucion: false, nombre_titulo: '', fecha_registro: new Date(), fecha_emision: new Date(), num_registro: '', carrera: this.carrera };
+  nuevoTituloEdit: Titulo = { id: 0, graduado: this.graduado, tipo: '', nivel: '', institucion: false, nombre_titulo: '', fecha_registro: new Date(), fecha_emision: new Date(), num_registro: '', carrera: this.carrera };
 
   editarClicked = false;
-
-  'titulo': Titulo;
-
-  onEditarClick(): void {
-    this.editarClicked = true;
-  }
-
-  onRegistrarClick(): void {
-    this.editarClicked = false;
-  }
-  titulos: any[] = [];
 
   constructor(private tituloService: TituloService) { }
 
@@ -29,31 +27,49 @@ export class TitulosComponent implements OnInit {
   }
 
   loadTitulos() {
-    // this.tituloService.getAllTitulos().subscribe(
-    //   (      data: any[]) => {
-    //     this.titulos = data;
-    //   },
-    //   (      error: any) => {
-    //     console.error('Error al cargar los títulos:', error);
-    //   }
-    // );
+    this.tituloService.getTitulos().subscribe(
+      titulos => this.titulos = titulos,
+      error => console.error(error)
+    );
   }
 
-  onUpdateClick(id: number) {
-    // Lógica para actualizar un título
-    // Puedes abrir el modal o realizar otras acciones según tu implementación
+  createTitulo() {
+    this.editarClicked = false;
+    this.tituloService.createTitulo(this.nuevoTitulo).subscribe(
+      sector => {
+        console.log('Titulo creado exitosamente:', sector);
+        this.loadTitulos();
+      },
+      error => console.error('Error al crear el titulo:', error)
+    );
+  }
+
+  onEditarClick(titulo: Titulo): void {
+    this.editarClicked = true;
+    this.nuevoTituloCarga = { ...titulo };
+  }
+
+  onRegistrarClick(): void {
+    this.editarClicked = false;
+  }
+
+  onUpdateClick() {
+    const id = this.nuevoTituloCarga.id;
+    if (id !== undefined) {
+      this.tituloService.updateTitulo(id, this.nuevoTituloCarga).subscribe(
+        tituloActualizado => {
+          console.log('Sector actualizado exitosamente:', tituloActualizado);
+          
+          this.loadTitulos();
+        },
+        error => console.error('Error al actualizar el titulo:', error)
+      );
+    } else {
+      console.error('Error: El ID del titulo es undefined.');
+    }
   }
 
   onDeleteClick(id: number) {
-    // Lógica para eliminar un título
-    this.tituloService.deleteTitulo(id).subscribe(
-      () => {
-        // Actualizar la lista después de la eliminación
-        this.loadTitulos();
-      },
-      error => {
-        console.error('Error al eliminar el título:', error);
-      }
-    );
+
   }
 }
