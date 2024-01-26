389a37473bfe7290849b9e6ee327da9bb80785c4
Lokyre <jose.fares.est@tecazuay.edu.ec>
2024-01-26 08:20:57 -0500

    A

--- src/app/pages/company/company-routing.module.ts
+++ src/app/pages/company/company-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     { path: 'postulaciones_form', component: OfertasLaboralesComponent },
     { path: 'empresas', component: CrudEmpresasComponent },
     { path: 'crud', component: EmpresasComponent },
-
+    {path: 'sector', component: ReferenciasEmpresarialesComponent},
 
 ];
 
