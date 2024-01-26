2c441df6f672c9bca0f0254e3a405f9387dc845f
Lokyre <jose.fares.est@tecazuay.edu.ec>
2024-01-26 09:12:50 -0500

    A

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-//import { SectorEmpresarialComponent } from './sector-empresarial/sector-empresarial.component';
+import { SectorEmpresarialComponent } from './sector-empresarial/sector-empresarial.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'perfil-usuario', component: PerfilUsuarioComponent },
-   // { path: 'sector-empresarial', component: SectorEmpresarialComponent}
+    { path: 'sector-empresarial', component: SectorEmpresarialComponent}
 ];
 
 @NgModule({
