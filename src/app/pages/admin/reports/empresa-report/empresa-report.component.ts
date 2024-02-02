15c942878cbb6d9940b909540b6c29bee831d349
Evelyn Chichande <echichandeperea@gmail.com>
2024-02-02 02:23:08 -0500

    more

--- src/app/pages/admin/reports/empresa-report/empresa-report.component.ts
+++ src/app/pages/admin/reports/empresa-report/empresa-report.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Graduado } from '../../../../data/model/graduado';
+import { GraduadoService } from '../../../../data/service/graduado.service';
 @Component({
   selector: 'app-empresa-report',
   templateUrl: './empresa-report.component.html',
   styleUrl: './empresa-report.component.css'
 })
-export class EmpresaReportComponent {
+export class EmpresaReportComponent implements OnInit{
 
+  graduadoList: Graduado[] = []
+
+  constructor(private graduadoService: GraduadoService) {}
+
+  ngOnInit(): void {
+    this.cargarLista()
+  }
+
+  cargarLista():void {
+    this.graduadoService.getGraduadosWithOutOferta().subscribe(
+      graduado => this.graduadoList = graduado
+    )
+  }
 }
