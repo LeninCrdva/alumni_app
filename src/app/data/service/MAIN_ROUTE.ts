ae43fe41253aea3d2afdb761fb5b19c9e39794c4
Miguel13fdf <miguel.haro.est@tecazuay.edu.ec>
2024-04-16 12:42:00 -0500

    Ruta de la nube de backend

--- src/app/data/service/MAIN_ROUTE.ts
+++ src/app/data/service/MAIN_ROUTE.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
   })
   export class MAIN_ROUTE {
   
-   static API_ENDPOINT = 'http://localhost:8080';
+   //static API_ENDPOINT = 'http://localhost:5000';
     //static API_ENDPOINT = 'http://alumnibacked23-env.eba-gemair7x.us-east-1.elasticbeanstalk.com';
+    static API_ENDPOINT = 'http://alumnibacked23-env-43.eba-gemair7x.us-east-1.elasticbeanstalk.com';
+    
     
   }
