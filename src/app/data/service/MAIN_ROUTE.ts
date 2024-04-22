70b00e5ccd3b32e6533c47e4e322c05b23242b1f
Miguel13fdf <miguel.haro.est@tecazuay.edu.ec>
2024-04-21 21:44:23 -0500

    Hay puedes probarle con backend en la nube

--- src/app/data/service/MAIN_ROUTE.ts
+++ src/app/data/service/MAIN_ROUTE.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class MAIN_ROUTE {
 
-  static API_ENDPOINT = 'http://localhost:5000';
-  //static API_ENDPOINT = ''http://alumnibacked23-env-43.eba-gemair7x.us-east-1.elasticbeanstalk.com'; <-- Esta es la URL de la API en AWS
-  //static API_ENDPOINT = 'http://localhost:8080';
+  //static API_ENDPOINT = 'http://localhost:5000';
+  //static API_ENDPOINT = 'http://alumnibacked23-env-43.eba-gemair7x.us-east-1.elasticbeanstalk.com'; <-- Esta es la URL de la API en AWS
+  static API_ENDPOINT = 'http://alumnibacked23-env.eba-gemair7x.us-east-1.elasticbeanstalk.com';
 }
